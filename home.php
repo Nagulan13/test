9131819a88f5d0a44e0ba0992f40396922ff8b39
Nagulan13 <nagu1370@gmail.com>
2025-02-27 17:02:33 +0800

    New page

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width= device-width, initial-scale=1.0">
+        <title>Dashboard</title>
+
+        <link href="styles.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="container">
+                <div class="navbar">
+                    <a href="#">Basic Column</a>
+                    <a href="#">Stacked Column</a>
+                    <a hreft="#">Multiple axes</a>
+                </div>
+                <div class="content">
+                    <div class="chart">
+
+                    
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
