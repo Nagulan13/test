3662637e9f6a8e3dd65170e21e4bb3260283a031
Nagulan13 <nagu1370@gmail.com>
2025-02-27 17:37:10 +0800

    asd

--- auth.php
+++ auth.php
@@ -1,17 +1,20 @@
-
-
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
-
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-echo $_POST["email"] . "<br>";
-echo $_POST["password"];
+    if ($email == "nagu1370@gmail.com" && $password == "nagu1370") {
+        echo "Login Success";
+        header("Location: home.php"); // Redirect to home
+        exit();
+    } else {
+        echo "Invalid Login";
+        header("Location: index.html"); // Back to Login Page
+        exit();
+    }
+} else {
+    echo "405 Not Allowed";
+}
 
 ?>
-
-</body>
-</html>
